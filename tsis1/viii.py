a8c69124439200ca1d7e26b8b419764aca2bd00e
Dimash <123285741+pp2-22B030439@users.noreply.github.com>
2023-02-01 23:18:41 +0600

    Add files via upload

---
+++ tsis1/viii.py
@@ -0,0 +1,21 @@
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
---
+++ tsis1/xii.py
@@ -0,0 +1,86 @@
+x = 5
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+print(x ** y)
+print(x // y)
+
+x = 5
+x += 3
+print(x)
+x -= 3
+print(x)
+x *= 3
+print(x)
+x /= 3
+print(x)
+x %= 3
+print(x)
+x **= 3
+print(x)
+x //= 3
+print(x)
+
+x = 5
+x &= 3
+print(x)
+x |= 3
+print(x)
+x ^= 3
+print(x)
+x >>= 3
+print(x)
+x <<= 3
+print(x)
+
+x = 5
+y = 3
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5
+print(x > 3 and x < 10)
+print(x > 3 or x < 10)
+print(not(x > 3 and x < 10))
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is z)
+print(x is y)
+print(x == y)
+
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
+print(x is not z)
+print(x is not y)
+print(x != y)
+
+x = ["apple", "banana"]
+print("banana" in x)
+
+x = ["apple", "banana"]
+print("pineapple" not in x)
+
+print(6 & 3)
+
+print(6 | 3)
+
+print(6 ^ 3)
+
+print(~3)
+
+print(3 << 2)
+
+print(8 >> 2)
+
+
