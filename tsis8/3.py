2c499c169281054a8b2420c852d609de765db09d
Dimash <123285741+pp2-22B030439@users.noreply.github.com>
2023-04-05 21:20:54 +0600

    Update 3.py

--- tsis8/3.py
+++ tsis8/3.py
@@ -1,119 +1,115 @@
 import pygame
 
+# инициализация Pygame
+pygame.init()
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    clock = pygame.time.Clock()
+# установка размеров экрана
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
 
-    radius = 15
-    x = 0
-    y = 0
-    mode = 'blue'
-    points = []
-    is_drawing_rect = False
-    rect_start_pos = None
-    rectangles = []
+# установка заголовка окна
+pygame.display.set_caption("Рисование")
 
-    while True:
+# установка цветовой палитры
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
 
-        pressed = pygame.key.get_pressed()
+# установка размера кисти
+brush_size = 10
 
-        alt_held = pressed[pygame.K_LALT] or pressed[pygame.K_RALT]
-        ctrl_held = pressed[pygame.K_LCTRL] or pressed[pygame.K_RCTRL]
+# установка начального цвета
+brush_color = BLACK
+drawing_type = "pen"
+# создание поверхности для рисования
+drawing = False
+last_pos = None
+canvas = pygame.Surface((screen_width, screen_height))
 
-        for event in pygame.event.get():
-
-            # determine if X was clicked, or Ctrl+W or Alt+F4 was used
-            if event.type == pygame.QUIT:
-                return
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w and ctrl_held:
-                    return
-                if event.key == pygame.K_F4 and alt_held:
-                    return
-                if event.key == pygame.K_ESCAPE:
-                    return
-
-                # determine if a letter key was pressed
-                if event.key == pygame.K_r:
-                    mode = 'red'
-                elif event.key == pygame.K_g:
-                    mode = 'green'
-                elif event.key == pygame.K_b:
-                    mode = 'blue'
-                elif event.key == pygame.K_d:
-                    # enter rectangle drawing mode
-                    is_drawing_rect = True
-                    rect_start_pos = pygame.mouse.get_pos()
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:  # left click grows radius
-                    radius = min(200, radius + 1)
-                elif event.button == 3:  # right click shrinks radius
-                    radius = max(1, radius - 1)
-
-            if event.type == pygame.MOUSEMOTION:
-                # if mouse moved and left button is held down, add point to list
-                if pygame.mouse.get_pressed()[0] and not is_drawing_rect:
-                    position = event.pos
-                    points = points + [position]
-                    points = points[-256:]
-
-            if event.type == pygame.MOUSEBUTTONUP:
-                if is_drawing_rect:
-                    # exit rectangle drawing mode and draw rectangle
-                    is_drawing_rect = False
-                    rect_end_pos = pygame.mouse.get_pos()
-                    rect = pygame.Rect(rect_start_pos,
-                                       (rect_end_pos[0] - rect_start_pos[0], rect_end_pos[1] - rect_start_pos[1]))
-                    rectangles.append(rect)
-
-        screen.fill((0, 0, 0))
-
-        # draw all rectangles
-        for rect in rectangles:
-            pygame.draw.rect(screen, (255, 255, 255), rect, 2)
-
-        # draw all points if not in rectangle drawing mode
-        if not is_drawing_rect:
-            i = 0
-            while i < len(points) - 1:
-                drawLineBetween(screen, i, points[i], points[i + 1], radius, mode)
-                i += 1
-
-        # if in rectangle drawing mode, draw rectangle preview
-        if is_drawing_rect:
-            rect_end_pos = pygame.mouse.get_pos()
-            rect = pygame.Rect(rect_start_pos,
-                               (rect_end_pos[0] - rect_start_pos[0], rect_end_pos[1] - rect_start_pos[1]))
-            pygame.draw.rect(screen, (255, 255, 255), rect, 2)
-
-        pygame.display.flip()
-
-        clock.tick(60)
-
-def drawLineBetween(screen, index, start, end, width, color_mode):
-    c1 = max(0, min(255, 2 * index - 256))
-    c2 = max(0, min(255, 2 * index))
-
-    if color_mode == 'blue':
-        color = (c1, c1, c2)
-    elif color_mode == 'red':
-        color = (c2, c1, c1)
-    elif color_mode == 'green':
-        color = (c1, c2, c1)
-
-    dx = start[0] - end[0]
-    dy = start[1] - end[1]
-    iterations = max(abs(dx), abs(dy))
-
-    for i in range(iterations):
-        progress = 1.0 * i / iterations
-        aprogress = 1 - progress
-        x = int(aprogress * start[0] + progress * end[0])
-        y = int(aprogress * start[1] + progress * end[1])
-        pygame.draw.circle(screen, color, (x, y), width)
+# основной цикл программы
+running = True
+while running:
+    # обработка событий клавиатуры
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                # выход из режима рисования
+                drawing = False
+            elif event.key == pygame.K_p:
+                # выбор ручки
+                drawing_type = "pen"
+            elif event.key == pygame.K_r:
+                # выбор рисования прямоугольника
+                drawing_type = "rect"
+            elif event.key == pygame.K_c:
+                # выбор рисования круга
+                drawing_type = "circle"
+            elif event.key == pygame.K_e:
+                # стирание холста
+                canvas.fill(BLACK)
+            elif event.key == pygame.K_s:
+                # сохранение изображения
+                pygame.image.save(canvas, "drawing.png")
+            elif event.key == pygame.K_MINUS:
+                # уменьшение размера кисти
+                brush_size = max(1, brush_size - 1)
+            elif event.key == pygame.K_PLUS or event.key == pygame.K_EQUALS:
+                # увеличение размера кисти
+                brush_size += 1
+            elif event.key == pygame.K_1:
+                # выбор цвета черный
+                brush_color = BLACK
+            elif event.key == pygame.K_2:
+                # выбор цвета красный
+                brush_color = RED
+            elif event.key == pygame.K_3:
+                # выбор цвета зеленый
+                brush_color = GREEN
+            elif event.key == pygame.K_4:
+                # выбор цвета синий
+                brush_color = BLUE
+            elif event.key == pygame.K_5:
+                # выбор цвета желтый
+                brush_color = YELLOW
 
 
-main()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            # начало рисования
+            drawing = True
+            last_pos = pygame.mouse.get_pos()
+        elif event.type == pygame.MOUSEBUTTONUP:
+            # конец рисования
+            drawing = False
+        elif event.type == pygame.MOUSEMOTION:
+            # рисование
+            # рисование фигуры
+            if drawing_type == "pen":
+                # рисование кистью
+                if drawing:
+                    current_pos = pygame.mouse.get_pos()
+                    pygame.draw.line(canvas, brush_color, last_pos, current_pos, brush_size)
+                    last_pos = current_pos
+
+        elif drawing_type == "rect":
+                # рисование прямоугольника
+            if drawing:
+                current_pos = pygame.mouse.get_pos()
+                pygame.draw.rect(canvas,brush_color, (last_pos[0], last_pos[1], current_pos[0] - last_pos[0], current_pos[1] - last_pos[1]))
+        elif drawing_type == "circle":
+                    # рисование круга
+            if drawing:
+                current_pos = pygame.mouse.get_pos()
+                radius = max(abs(current_pos[0] - last_pos[0]), abs(current_pos[1] - last_pos[1]))
+                pygame.draw.circle(canvas, brush_color, last_pos, radius, brush_size)
+                                     # отрисовка экрана
+    screen.blit(canvas, (0, 0))
+    pygame.display.flip()
+
+# завершение Pygame
+pygame.quit()
